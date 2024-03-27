2181afbc934c079fcf23a46324a85784f9ee839d
aibakeneko <aibakeneko@gmail.com>
2024-03-27 15:57:32 +0100

    Added function: extract_n_consecutive_sentences Updated create_jsonl_dataset: Changed argument tokens_per_file to sentences_per_file

--- proceedings_curation/scripts/create_jsonl_dataset.py
+++ proceedings_curation/scripts/create_jsonl_dataset.py
@@ -1,8 +1,10 @@
 import os
+import random
 import re
 from typing import Optional
 
 import jsonlines
+import nltk
 import pandas as pd
 import typer
 from dotenv import load_dotenv
@@ -18,10 +20,10 @@ def create_jsonl_dataset(
     output_path: str,
     dataset_name: Annotated[Optional[str], typer.Argument()] = 'dataset',
     number_of_files: Annotated[Optional[int], typer.Argument()] = None,
-    tokens_per_file: Annotated[Optional[int], typer.Argument()] = None,
+    sentences_per_file: Annotated[Optional[int], typer.Argument()] = None,
     seed: Annotated[Optional[int], typer.Option(help="Random seed for sampling")] = None,
 ) -> None:
-    """Create a JSONL dataset from a folder of text files using a metadata index. Create a sample of the dataset by specifying the number of files to include and the number of tokens to keep per file.
+    """Create a JSONL dataset from a folder of text files using a metadata index. Create a sample of the dataset by specifying the number of files to include and the number of sentences per file to keep. Use a random seed for reproducibility.
 
     The metadata index is a CSV file with the following columns:
     - filename: Name of the PDF file
@@ -50,7 +52,7 @@ def create_jsonl_dataset(
         output_path (str): Output path for the JSONL dataset
         dataset_name (str, optional): Name of the dataset. Defaults to 'dataset'.
         number_of_files (int, optional): Number of files to sample. Defaults to None.
-        tokens_per_file (int, optional): Number of tokens to keep per file. Defaults to None.
+        sentences_per_file (int, optional): Number of sentences to keep per file. Defaults to None.
         seed (int, optional): Random seed for sampling. Defaults to 42.
     """
     # Load metadata index
@@ -80,8 +82,8 @@ def create_jsonl_dataset(
             with open(os.path.join(input_path, filename), 'r', encoding='utf-8') as file:
                 text = file.read()
                 text = remove_unusual_line_terminators(text)
-                if tokens_per_file is not None:
-                    text = ' '.join(text.split()[:tokens_per_file])
+                if sentences_per_file is not None:
+                    text = extract_n_consecutive_sentences(text, sentences_per_file, seed=seed)
             meta = {
                 'file': filename,
                 'title': row.title_meeting,
@@ -103,6 +105,31 @@ def remove_unusual_line_terminators(text: str) -> str:
     pattern = re.compile(r'[\x00-\x08\x0E-\x1F\u0085\u2028\u2029]')
     cleaned_text = pattern.sub('', text)
     return cleaned_text
+
+
+def extract_n_consecutive_sentences(text: str, n: int, seed: Optional[int] = None) -> str:
+    """Return a string with n consecutive sentences extracted from a text. If n is greater than the number of sentences in the text, return the full text.
+
+    Args:
+        text (str): The input text
+        n (int): The number of sentences to extract
+        seed (Optional[int], optional): Random seed for sampling. Defaults to None.
+
+    Returns:
+        str: A substring of the input text with n consecutive sentences randomly selected
+    """
+    sentences = nltk.sent_tokenize(text)
+
+    if n >= len(sentences):
+        return text
+
+    if seed is not None:
+        random.seed(seed)
+
+    start_index = random.randint(0, len(sentences) - n)
+    extracted_sentences = sentences[start_index : start_index + n]
+
+    return ' '.join(extracted_sentences)
 
 
 if __name__ == "__main__":
