c5d2eedb7b760d906e4d23c2bfa053483f4e4edd
aibakeneko <aibakeneko@gmail.com>
2024-09-17 14:13:33 +0200

    Added caplog fixture

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+from typing import Any, Generator
+
+import pytest
+from _pytest.logging import LogCaptureFixture
+from loguru import logger
+
+
+@pytest.fixture(name="caplog")
+def caplog_fixture(caplog: LogCaptureFixture) -> Generator[Any, Any, Any]:
+    """See: https://loguru.readthedocs.io/en/stable/resources/migration.html#replacing-caplog-fixture-from-pytest-library"""
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False,  # Set to 'True' if your test is spawning child processes.
+    )
+    yield caplog
+    logger.remove(handler_id)
